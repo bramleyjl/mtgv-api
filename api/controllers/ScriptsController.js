d178c3bf5c97f85f7a132ef7b64b0f43e2cfed8e
John Bramley <bramleyjl@gmail.com>
2018-04-06 14:10:26 -0400

    multiple card lookup added

--- api/controllers/ScriptsController.js
+++ api/controllers/ScriptsController.js
@@ -4,26 +4,30 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-let scripts = require('../models/Scripts')
+let scripts = require('../models/Scripts');
+var Promise = require("bluebird");
 
 module.exports = {
 	cardLookup: function(req, res) {
-
 		const nameFilter = /\[.*?\]/ig;
-		const cardNames = req.body.script.match(nameFilter);
-		//remove captured brackets to leave clean names
-		var newCardNames = new Array;
-		for (cards of cardNames) {
-			newCardNames.push(cards.substring(1, cards.length -1));
-		}
-		console.log(newCardNames)
+		const lookupNames = req.body.script.match(nameFilter);
 
-		var imageLink = scripts.cardLookup(newCardNames)
-		imageLink.then( function(result) {
-			const cardImages = result;
+		//remove captured brackets to leave clean names
+		var cardNames = new Array;
+		for (card of lookupNames) {
+			let trimmedName = 
+			cardNames.push(card.substring(1, card.length -1));
+		}
+		
+		let cardImages = new Array;
+		Promise.map(cardNames, function(name) {
+			return scripts.imageLookup(name);
+		}, {concurrency: 1})
+		.then(function(results) {
 			return res.view('pages/results', {
-				cardImages: cardImages
+				cardImages: results
 			});
-		});
+		})
 	}
+
 };
--- api/models/Scripts.js
+++ api/models/Scripts.js
@@ -25,24 +25,21 @@ module.exports = {
     //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
 
   },
-    cardLookup : function(cardArray) {
-        for (card of cardArray) {
-            return axios.get(`https://api.scryfall.com/cards/named?fuzzy=${card}`)
-            .then(response => {
-                let allEditions = response.data.prints_search_uri
-                return axios.get(allEditions)
-                .then(response => {
-                    const editionImages = new Array;
-                    for (edition of response.data.data) {
-                        editionImages.push(edition.image_uris.small)
-                    }
-                    return editionImages;
-                })
-            })
-            .catch(error => {
-                console.log(error);
-            })
-        }
+    imageLookup : function(card) {
+        return axios.get(`https://api.scryfall.com/cards/named?fuzzy=${card}`)
+        .then(response => {
+            const allEditions = response.data.prints_search_uri
+            return axios.get(allEditions)
+        })
+        .then(response => {
+            let editionImages = new Array;
+            for (edition of response.data.data) {
+                editionImages.push(edition.image_uris.small)
+            }
+            return editionImages;
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
-
 };
