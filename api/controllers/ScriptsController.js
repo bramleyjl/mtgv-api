efa900a31c2887846c6048d49310c56163b5cb5f
John Bramley <bramleyjl@gmail.com>
2018-04-12 09:19:30 -0400

    onclick image removal begun. current function is click to remove image, needs to be changed to click to remove all unclicked images

--- api/controllers/ScriptsController.js
+++ api/controllers/ScriptsController.js
@@ -1,16 +1,12 @@
-/**
- * ScriptsController
- *
- * @description :: Server-side actions for handling incoming requests.
- * @help        :: See https://sailsjs.com/docs/concepts/actions
- */
-let scripts = require('../models/Scripts');
-var Promise = require("bluebird");
+let Scripts = require('../models/Scripts');
+let Promise = require('bluebird');
 
 module.exports = {
 	cardLookup: function(req, res) {
+		//parse script input for all card names and add them to an array
+		const script = req.body.script
 		const nameFilter = /\[.*?\]/ig;
-		const lookupNames = req.body.script.match(nameFilter);
+		const lookupNames = script.match(nameFilter);
 
 		//remove captured brackets to leave clean names
 		var cardNames = new Array;
@@ -21,11 +17,15 @@ module.exports = {
 		
 		let cardImages = new Array;
 		Promise.map(cardNames, function(name) {
-			return scripts.imageLookup(name);
+			return Scripts.imageLookup(name);
 		}, {concurrency: 1})
 		.then(function(results) {
-			return res.view('pages/results', {
-				cardImages: results
+			
+			//console.log(results)
+			
+			res.view('pages/imageSelect', {
+				cardImages: results,
+				baseScript: script
 			});
 		})
 	}
