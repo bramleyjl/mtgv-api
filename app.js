6bd90a676b878c1494499bf57cf2ad7ca540d75a
John Bramley <bramleyjl@gmail.com>
2018-04-13 16:18:45 -0400

    transition from Sails to Express

--- app.js
+++ app.js
@@ -1,54 +1,61 @@
-/**
- * app.js
- *
- * Use `app.js` to run your app without `sails lift`.
- * To start the server, run: `node app.js`.
- *
- * This is handy in situations where the sails CLI is not relevant or useful,
- * such as when you deploy to a server, or a PaaS like Heroku.
- *
- * For example:
- *   => `node app.js`
- *   => `npm start`
- *   => `forever start app.js`
- *   => `node debug app.js`
- *
- * The same command-line arguments and env vars are supported, e.g.:
- * `NODE_ENV=production node app.js --port=80 --verbose`
- *
- * For more information see:
- *   https://sailsjs.com/anatomy/app.js
- */
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
+var routes = require('./routes/index');
 
-// Ensure we're in the project directory, so cwd-relative paths work as expected
-// no matter where we actually lift from.
-// > Note: This is not required in order to lift, but it is a convenient default.
-process.chdir(__dirname);
+var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Attempt to import `sails` dependency, as well as `rc` (for loading `.sailsrc` files).
-var sails;
-var rc;
-try {
-  sails = require('sails');
-  rc = require('sails/accessible/rc');
-} catch (err) {
-  console.error('Encountered an error when attempting to require(\'sails\'):');
-  console.error(err.stack);
-  console.error('--');
-  console.error('To run an app using `node app.js`, you need to have Sails installed');
-  console.error('locally (`./node_modules/sails`).  To do that, just make sure you\'re');
-  console.error('in the same directory as your app and run `npm install`.');
-  console.error();
-  console.error('If Sails is installed globally (i.e. `npm install -g sails`) you can');
-  console.error('also run this app with `sails lift`.  Running with `sails lift` will');
-  console.error('not run this file (`app.js`), but it will do exactly the same thing.');
-  console.error('(It even uses your app directory\'s local Sails install, if possible.)');
-  return;
-}//-•
+app.use('/', routes);
 
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-// Start server
-sails.lift(rc('sails'));
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err,
+            status: err.status
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {},
+        status: err.status
+    });
+});
+
+app.listen(3000);
+console.log('MTGScriptAutomater listening on port 3000.');
+
+module.exports = app;
---
+++ models/scripts.js
@@ -0,0 +1,36 @@
+let axios = require('axios');
+
+module.exports = {
+    imageLookup : function(card) {
+        return axios.get(`https://api.scryfall.com/cards/named?fuzzy=${card}`)
+        .then(response => {
+            const allEditions = response.data.prints_search_uri
+            return axios.get(allEditions)
+        })
+        .then(response => {
+            let editionImages = new Array;
+            for (edition of response.data.data) {
+                editionImages.push(edition.image_uris.small)
+            }
+            return editionImages;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    },
+    hiRezDownload: function(name, link) {
+        const oldLink = link;
+        link = link.replace('small', 'png');
+        link = link.replace('jpg', 'png');
+        return axios.get(link)
+        .then(response => {
+            let downloadLink = new Object;
+            downloadLink[name] = link;
+            downloadLink[oldLink] = oldLink;
+            return downloadLink;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+};
