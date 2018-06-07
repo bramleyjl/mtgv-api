d24ae8e9fce36d47ba57bf4ea2fe86d6d5ef5873
John Bramley <bramleyjl@gmail.com>
2018-06-06 23:14:54 -0400

    script entry basic HTML and fill premade text function ported to react

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/index.css';
-import ScriptEntry from './components/ScriptEntry';
+import HomePage from './components/HomePage';
 import ImagePicker from './components/ImagePicker';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -11,7 +12,7 @@ const Root = () => {
   return (
     <BrowserRouter>
       <div>
-        <Route exact path='/' component={ScriptEntry} />
+        <Route exact path='/' component={HomePage} />
         <Route path='/images' component={ImagePicker} />
       </div>
     </BrowserRouter>
