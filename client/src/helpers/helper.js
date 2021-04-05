18d52ddf19016f37bf85f120a58b581837c5b8c6
John Bramley <bramleyjl@gmail.com>
2021-04-04 20:46:21 -0400

    adds missing InfoPopover component and changes localStorage of cardlist to sessionStorage

--- client/src/helpers/helper.js
+++ client/src/helpers/helper.js
@@ -1,14 +1,14 @@
 export const getCachedData = (field, data = '') => {
   if (data !== '') {
-    localStorage.setItem(field, data);
+    sessionStorage.setItem(field, data);
   } else {
-    data = localStorage.getItem(field);
+    data = sessionStorage.getItem(field);
   }
   return data;
 };
 
 export const setCachedData = (field, data) => {
-  localStorage.setItem(field, data);
+  sessionStorage.setItem(field, data);
 };
 
 export const sortVersions = (cardImages, sortMethod) => {
