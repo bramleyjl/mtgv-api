6d10ef90c9a77ac38efd9b20b73d7d4fbb5d3303
John Bramley <bramleyjl@gmail.com>
2023-05-15 08:06:16 -0600

    mui theme update

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 import "typeface-roboto";
 import "./css/index.css";
 import Root from "./components/Root";
 import registerServiceWorker from "./registerServiceWorker";
 
-const theme = createTheme({
+const theme = createMuiTheme({
   palette: {
     primary: {
       main: '#b1b1b1',
@@ -22,14 +22,14 @@ const theme = createTheme({
       dark: '#8aacc8',
       contrastText: '#000000'
     },
-  },
+  }
 });
 
 const App = () => {
   return (
-    <MuiThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <Root />
-    </MuiThemeProvider>
+    </ThemeProvider>
   );
 };
 
